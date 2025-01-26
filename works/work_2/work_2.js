190f2579c723f2b327a8f4963a18a06462ecdde6
alastairrosso <alastair.gy@gmail.com>
2025-01-25 20:37:18 -0500

    W2: Replace grids/fields with more convenient staggered version, no velocity advection yet

--- works/work_2/work_2.js
+++ works/work_2/work_2.js
@@ -11,89 +11,88 @@ cnv.height = workOptions.height - (Math.floor(workOptions.height) % cellSize);
 cnv.style.width = cnv.width + "px";
 cnv.style.height = cnv.height + "px";
 
-const gridWidth = cnv.width / cellSize;
-const gridHeight = cnv.height / cellSize;
+const fieldWidth = cnv.width / cellSize;
+const fieldHeight = cnv.height / cellSize;
 
 let paused = true;
 
-// Grid
-// NOTE: when displayed, grid uses non-inverted y-axis (origin in bottom-left)
-const gridSize = gridWidth * gridHeight;
-let grid = new Uint32Array(gridSize);
-let solids = new Uint32Array(gridSize);   // 0 = solid/wall; 1 = open/fluid
-let u_field_prev = null;
-let v_field_prev = null;
-let u_field = new Float32Array(gridSize); // horizontal components for each cell
-let v_field = new Float32Array(gridSize); //   vertical components for each cell
+// Fields
+// NOTE: when displayed, fields use non-inverted y-axis (origin in bottom-left)
+const fieldSize = fieldWidth * fieldHeight;
+let field = new Uint32Array(fieldSize);     // holds cell densities
+let solids = new Uint32Array(fieldSize);    // 0 = solid/wall; 1 = open/fluid
 
-setCell = (g, x, y, val) => {
-    // below commented lines used for wrapping environment to opposite border
-    // x = ((x % gridWidth) + gridWidth) % gridWidth;
-    // y = ((y % gridHeight) + gridHeight) % gridHeight;
-    g[y * gridWidth + x] = val;
-}
+// staggered grid sizes for u and v fields
+const u_field_width = fieldSize + 1;
+const u_field_height = fieldSize;
+const u_field_size = u_field_width*u_field_height;
+const v_field_width = fieldSize;
+const v_field_height = fieldSize + 1;
+const v_field_size = v_field_width*v_field_height;
 
-getCell = (g, x, y) => {
-    // x = ((x % gridWidth) + gridWidth) % gridWidth;
-    // y = ((y % gridHeight) + gridHeight) % gridHeight;
-    return g[y * gridWidth + x];
-}
+let u_field = new Float32Array(u_field_size); // horizontal components for each cell
+let v_field = new Float32Array(v_field_size); //   vertical components for each cell
+let u_field_next = new Float32Array(u_field_size);
+let v_field_next = new Float32Array(v_field_size);
+
+setCell = (fd, x, y, data) => {fd[y * fieldWidth + x] = data};
+getCell = (fd, x, y) => fd[y * fieldWidth + x];
+setUCell = (fd, x, y, data) => {fd[y * u_field_width + x] = data};
+getUCell = (fd, x, y) => fd[y * u_field_width + x];
+setVCell = (fd, x, y, data) => {fd[y * v_field_width + x] = data};
+getVCell = (fd, x, y) => fd[y * v_field_width + x];
 
 // --- SIMULATION CODE ---
 
 const dt = 1 / 60.0; // delta t = time step
 const overrelax = 1.9;
-const edge_left  = (x, y) => 0.5 * (getCell(u_field_prev, x, y) + getCell(u_field_prev, x-1, y));
-const edge_right = (x, y) => 0.5 * (getCell(u_field_prev, x, y) + getCell(u_field_prev, x+1, y));
-const edge_up    = (x, y) => 0.5 * (getCell(v_field_prev, x, y) + getCell(v_field_prev, x, y+1));
-const edge_down  = (x, y) => 0.5 * (getCell(v_field_prev, x, y) + getCell(v_field_prev, x, y-1));
 
 // maintain div(u) = 0
 function solveDivergence() {
-    u_field_prev = u_field;
-    v_field_prev = v_field;
+    u_field_next.set(u_field);
+    v_field_next.set(v_field);
 
-    for (let y = 1; y < gridHeight-1; ++y) {
-        for (let x = 1; x < gridWidth-1; ++x) {
-            if (getCell(solids, x, y) == 1) {
-                const div = overrelax * calcDivergence(x, y);
-                const neighbors = countSolids(x, y);
+    for (let y = 1; y < fieldHeight-1; ++y) {
+        for (let x = 1; x < fieldWidth-1; ++x) {
+            if (getCell(solids, x, y) == 0) continue;
 
-                const left  = edge_left(x, y)  + div*(getCell(solids, x-1, y) / neighbors);
-                const right = edge_right(x, y) - div*(getCell(solids, x+1, y) / neighbors);
-                const down  = edge_down(x, y)  + div*(getCell(solids, x, y-1) / neighbors);
-                const up    = edge_up(x, y)    - div*(getCell(solids, x, y+1) / neighbors);
+            const u_left  = getUCell(u_field_next, x,   y);
+            const u_right = getUCell(u_field_next, x+1, y);
+            const v_up    = getVCell(v_field_next, x,   y+1);
+            const v_down  = getVCell(v_field_next, x,   y);
+            const div = overrelax * (u_right - u_left + v_up - v_down);
+            
+            const cell_left  = getCell(solids, x-1, y);
+            const cell_right = getCell(solids, x+1, y);
+            const cell_up    = getCell(solids, x, y+1);
+            const cell_down  = getCell(solids, x, y-1);
+            const neighbors = cell_left + cell_right + cell_up + cell_down;
 
-                const u_new = 0.5 * (left + right);
-                const v_new = 0.5 * (up + down);
-                setCell(u_field, x, y, u_new);
-                setCell(v_field, x, y, v_new);
+            const du_left  = div * (cell_left  / neighbors);
+            const du_right = div * (cell_right / neighbors);
+            const dv_up    = div * (cell_up    / neighbors);
+            const dv_down  = div * (cell_down  / neighbors);
 
-                setCell(grid, x, y, density2color(calcDensity(x, y)));
-            }
+            setUCell(u_field_next, x,   y,   getUCell(u_field_next, x,   y)   + du_left);
+            setUCell(u_field_next, x+1, y,   getUCell(u_field_next, x+1, y)   - du_right);
+            setVCell(v_field_next, x,   y+1, getVCell(v_field_next, x,   y+1) - dv_up);
+            setVCell(v_field_next, x,   y,   getVCell(v_field_next, x,   y)   + dv_down);
         }
     }
-}
 
-function calcDivergence(x, y) {
-    const left  = edge_left(x, y);
-    const right = edge_right(x, y);
-    const up    = edge_up(x, y);
-    const down  = edge_down(x, y);
-    return right - left + up - down;
-}
-
-function countSolids(x, y) {
-    const left  = getCell(solids, x-1, y);
-    const right = getCell(solids, x+1, y);
-    const up    = getCell(solids, x, y+1);
-    const down  = getCell(solids, x, y-1);
-    return left + right + up + down;
+    u_field.set(u_field_next);
+    v_field.set(v_field_next);
 }
 
 function calcDensity(x, y) {
-    const u = getCell(u_field, x, y);
-    const v = getCell(v_field, x, y);
+    const u_left  = getUCell(u_field, x,   y);
+    const u_right = getUCell(u_field, x+1, y);
+    const v_up    = getVCell(v_field, x,   y+1);
+    const v_down  = getVCell(v_field, x,   y);
+
+    const u = 0.5 * (u_left + u_right);
+    const v = 0.5 * (v_up + v_down);
+
     const res = Math.hypot(u, v);
     const u_norm = Math.sign(u) * Math.min(Math.abs(u / res), Math.abs(u));
     const v_norm = Math.sign(v) * Math.min(Math.abs(v / res), Math.abs(v));
@@ -101,93 +100,24 @@ function calcDensity(x, y) {
 }
 
 function density2color(density) {
-    let tri = 0xff * density;
+    const tri = 0xff * density;
     return (tri << 16) | (tri << 8) | tri;
 }
 
-// move velocities within grid/field
-// TODO: doesn't actually work
+// move velocities within field
 function advectVelocities() {
-    u_field_prev = u_field;
-    v_field_prev = v_field;
+    
 
-    for (let y = 1; y < gridHeight-1; ++y) {
-        for (let x = 1; x < gridWidth-1; ++x) {
-            if (getCell(solids, x, y) == 1) {
-                // get previous velocity's on-screen position
-                const u_coord_x = x*cellSize - (0.5*cellSize);
-                const v_coord_y = y*cellSize - (0.5*cellSize);
-                const u_prev_coord_x = u_coord_x - dt*getCell(u_field_prev, x, y);
-                const v_prev_coord_y = v_coord_y - dt*getCell(v_field_prev, x, y);
-
-                const vel_prev = interpolateVelocity(u_prev_coord_x, v_prev_coord_y);
-                setCell(u_field, x, y, vel_prev[0]);
-                setCell(v_field, x, y, vel_prev[1]);
-            }
+    for (let y = 1; y < fieldHeight-1; ++y) {
+        for (let x = 1; x < fieldWidth-1; ++x) {
+            if (getCell(solids, x, y) == 0) continue;
         }
     }
 }
 
 // unoptimized, task for future me
 function interpolateVelocity(coord_x, coord_y) {
-    // vel1  |  vel2
-    // ------|------
-    // vel3  |  vel4
-
-    // find vel1, vel2, vel3, vel4 indices (x, y)
-    const vel1_x = Math.floor((coord_x - cellSize/2) / cellSize);
-    const vel1_y = Math.ceil((coord_y - cellSize/2) / cellSize);
-
-    const vel2_x = Math.ceil((coord_x - cellSize/2) / cellSize);
-    const vel2_y = Math.ceil((coord_y - cellSize/2) / cellSize);
-
-    const vel3_x = Math.floor((coord_x - cellSize/2) / cellSize);
-    const vel3_y = Math.floor((coord_y - cellSize/2) / cellSize);
-
-    const vel4_x = Math.ceil((coord_x - cellSize/2) / cellSize);
-    const vel4_y = Math.floor((coord_y - cellSize/2) / cellSize);
-
-    // get u_field and v_field values for vel1, vel2, vel3, vel4
-    const vel1_u = getCell(u_field, vel1_x, vel1_y);
-    const vel1_v = getCell(v_field, vel1_x, vel1_y);
-
-    const vel2_u = getCell(u_field, vel2_x, vel2_y);
-    const vel2_v = getCell(v_field, vel2_x, vel2_y);
-
-    const vel3_u = getCell(u_field, vel3_x, vel3_y);
-    const vel3_v = getCell(v_field, vel3_x, vel3_y);
-
-    const vel4_u = getCell(u_field, vel4_x, vel4_y);
-    const vel4_v = getCell(v_field, vel4_x, vel4_y);
-
-    // calc vel1, vel2, vel3, vel4 coords
-    // (only 2 of these are actually needed)
-    const vel1_coord_x = vel1_x*cellSize + cellSize/2;
-    // const vel1_coord_y = vel1_y*cellSize + cellSize/2;
-
-    // const vel2_coord_x = vel2_x*cellSize + cellSize/2;
-    // const vel2_coord_y = vel2_y*cellSize + cellSize/2;
-
-    // const vel3_coord_x = vel3_x*cellSize + cellSize/2;
-    const vel3_coord_y = vel3_y*cellSize + cellSize/2;
-
-    // const vel4_coord_x = vel4_x*cellSize + cellSize/2;
-    // const vel4_coord_y = vel4_y*cellSize + cellSize/2;
-
-    // weight vel1, vel2, vel3, vel4; average them; return;
-    const dx = coord_x - vel1_coord_x;
-    const dy = coord_y - vel3_coord_y;
-    const wx = dx / cellSize;
-    const wy = dy / cellSize;
-    const wx_inv = 1 - wx;
-    const wy_inv = 1 - wy;
-
-    const u_prev = vel1_u*wx_inv*wy     + vel2_u*wx*wy
-                 + vel3_u*wx_inv*wy_inv + vel4_u*wx*wy_inv;
-    const v_prev = vel1_v*wx_inv*wy     + vel2_v*wx*wy
-                 + vel3_v*wx_inv*wy_inv + vel4_v*wx*wy_inv;
-    
-    return [u_prev, v_prev];
+    //
 }
 
 // --- SIM LOOP FUNCTIONS ---
@@ -199,30 +129,24 @@ function initSim() {
         solids[i] = 1;
 
     // border cells
-    for (let x = 0; x < gridWidth; ++x) {
+    for (let x = 0; x < fieldWidth; ++x) {
         setCell(solids, x, 0, 0);
-        setCell(grid,   x, 0, 0x000000);
-        setCell(solids, x, gridHeight - 1, 0);
-        setCell(grid,   x, gridHeight - 1, 0x000000);
+        setCell(field,   x, 0, 0x000000);
+        setCell(solids, x, fieldHeight - 1, 0);
+        setCell(field,   x, fieldHeight - 1, 0x000000);
     }
 
-    for (let y = 0; y < gridHeight; ++y) {
+    for (let y = 0; y < fieldHeight; ++y) {
         setCell(solids, 0, y, 0);
-        setCell(grid,   0, y, 0x000000);
-        setCell(solids, gridWidth - 1, y, 0);
-        setCell(grid,   gridWidth - 1, y, 0x000000);
+        setCell(field,   0, y, 0x000000);
+        setCell(solids, fieldWidth - 1, y, 0);
+        setCell(field,   fieldWidth - 1, y, 0x000000);
     }
 
     // turbine
-    setCell(u_field, 0, 15, dt*100.0);
-    setCell(u_field, 0, 16, dt*100.0);
-    setCell(u_field, 0, 17, dt*100.0);
-    setCell(u_field, 0, 18, dt*100.0);
-    setCell(u_field, 0, 19, dt*100.0);
-
-    // just some extra starting force
-    setCell(u_field, 10, 25, dt*500.0);
-    setCell(v_field, 10, 25, dt*500.0);
+    setUCell(u_field, 1, 15, 10.0);
+    // setCell(u_field, fieldWidth-2, 35, -10.0);
+    // setCell(v_field, fieldWidth-2, 35,   5.0);
 }
 
 function drawCell(x, y, val) {
@@ -237,37 +161,26 @@ function drawCell(x, y, val) {
 function render() {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-    // render grid cells
-    for (let y = 0; y < gridHeight; ++y) {
-        for (let x = 0; x < gridWidth; ++x) {
-            drawCell(x, y, getCell(grid, x, y));
+    // render field cells
+    for (let y = 0; y < fieldHeight; ++y) {
+        for (let x = 0; x < fieldWidth; ++x) {
+            drawCell(x, y, getCell(field, x, y));
         }
     }
 }
 
 // simulate fluid
 function simulate() {
-    // generate achromatic palette (just visualizing grid for now)
-    // let palette = 0x000000;
-    // let count = 0;
-
-    // for (let y = 0; y < gridHeight; ++y) {
-    //     for (let x = 0; x < gridWidth; ++x) {
-    //         if (getCell(solids, x, y) == 0) continue;
-    //         // NOTE: grid array holds color hex values for now, densities later
-    //         setCell(grid, x, y, palette);
-    //         let tri = (palette & 0xff);
-    //         if (count % 30 == 0) tri += 0x02;
-    //         palette = (tri << 16) | (tri << 8) | tri;
-    //         count++;
-    //     }
-    // }
-
-    // actual process
-
     // addForces();
     solveDivergence();
-    advectVelocities();
+    // advectVelocities();
+
+    // calculate, store densities
+    for (let y = 1; y < fieldHeight-1; ++y) {
+        for (let x = 1; x < fieldWidth-1; ++x) {
+            setCell(field, x, y, density2color(calcDensity(x, y)));
+        }
+    }
 }
 
 function update() {
